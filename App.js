9d4cdbe7f199ca0344f2ad11cdbeba3948eb3e5d
baejoonsoo <baejoonsoo21@gmail.com>
2022-01-14 03:44:04 +0900

    Style: loding style

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ export default function App() {
         contentContainerStyle={styles.weather}
       >
         {days.length === 0 ? (
-          <View style={styles.day}>
+          <View style={{ ...styles.day, alignItems: "center" }}>
             <ActivityIndicator color="black" size="large" />
           </View>
         ) : (
